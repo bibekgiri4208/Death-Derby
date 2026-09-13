b8db640a11a66e569bf721e153d3dce121b55aa7
Bibek Giri <giribibek01@gmail.com>
2026-09-13 21:02:35 +0545

    Created Animated Popup effect in the Kill Counter

--- Assets/Scripts/UI/KillCounter.cs
+++ Assets/Scripts/UI/KillCounter.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -7,7 +8,20 @@ public class KillCounter : MonoBehaviour
     [SerializeField] private TextMeshProUGUI killCountText;
     [SerializeField] private string textPrefix = "";
 
+    [Header("Pop Animation")]
+    [Min(0.05f)]
+    [SerializeField] private float popDuration = 0.35f;
+    [Min(1f)]
+    [SerializeField] private float popScale = 1.35f;
+    [SerializeField] private AnimationCurve popCurve = new AnimationCurve(
+        new Keyframe(0f, 0f),
+        new Keyframe(0.25f, 1f),
+        new Keyframe(0.5f, 1.1f),
+        new Keyframe(1f, 0f));
+
     private int killCount;
+    private Vector3 baseScale;
+    private Coroutine popRoutine;
 
     private void OnEnable()
     {
@@ -17,10 +31,19 @@ public class KillCounter : MonoBehaviour
     private void OnDisable()
     {
         ZombieAI.OnZombieKilled -= OnZombieKilled;
+        if (popRoutine != null)
+        {
+            StopCoroutine(popRoutine);
+            popRoutine = null;
+        }
+        if (killCountText != null)
+            killCountText.transform.localScale = baseScale;
     }
 
     private void Start()
     {
+        if (killCountText != null)
+            baseScale = killCountText.transform.localScale;
         UpdateText();
     }
 
@@ -28,11 +51,34 @@ public class KillCounter : MonoBehaviour
     {
         killCount++;
         UpdateText();
+        PlayPopAnimation();
     }
 
     private void UpdateText()
     {
         if (killCountText != null)
             killCountText.text = textPrefix + killCount.ToString();
+    }
+
+    private void PlayPopAnimation()
+    {
+        if (popRoutine != null)
+            StopCoroutine(popRoutine);
+        popRoutine = StartCoroutine(PopAnimation());
+    }
+
+    private IEnumerator PopAnimation()
+    {
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.unscaledDeltaTime / popDuration;
+            float eased = popCurve.Evaluate(t);
+            float factor = 1f + (popScale - 1f) * eased;
+            killCountText.transform.localScale = baseScale * factor;
+            yield return null;
+        }
+        killCountText.transform.localScale = baseScale;
+        popRoutine = null;
     }
 }
